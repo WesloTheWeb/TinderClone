3ef36e92fedd0f591b55f5d4490a84be0894e5ba
Wesley <wesleyadwebster@gmail.com>
2022-05-14 23:13:27 -0500

    fixes

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return (
+        <nav>Hi</nav>
+    );
+};
+
+export default Navbar
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+import Navbar from '../components/Navbar/Navbar';
 
 const Home = () => {
+
+    const [authToken, isAuthToken] = useState(true);
+
+    const handleClick = () => {
+        console.log('Clicked');
+    }
+
+    useEffect(() => {
+        isAuthToken(true);
+    }, []);
+
     return (
-        <div>Home</div>
+        <>
+            <Navbar />
+            <div>
+                <h1 className="home">Swipe Right</h1>
+                <button className="primary-button" onClick={handleClick}>
+                    {
+                        authToken ? 'Signout' : 'Create Account'
+                    }
+                </button>
+            </div>
+        </>
     );
 };
 
