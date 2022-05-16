16569c17ee507d09b739760fd35de2844f53c717
Wesley <wesleyadwebster@gmail.com>
2022-05-16 16:59:25 -0500

    files added for logo and navbar work

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import tinderLogo1 from './tinder-logo.png';
+import tinderLogo2 from './tinder-logo-white.jpg';
+
+export { tinderLogo1 };
+export { tinderLogo2 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { tinderLogo1, tinderLogo2 } from '../../assets/index';
+import classes from './Navbar.module.scss';
+
+const { logo } = classes;
 
 const Navbar = (props) => {
+
+    const minimal = true;
+
     return (
-        <nav>Hi</nav>
+        <nav>
+            <div>
+                <img className={logo} src={minimal ? tinderLogo1 : tinderLogo2} alt="tinder logo" />
+            </div>
+        </nav>
     );
 };
 
