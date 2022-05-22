63b8a2c5a182023200655efdf019227c738247af
Wesley <wesleyadwebster@gmail.com>
2022-05-22 17:13:19 -0500

    initial commit

---
+++ src/components/AuthModal/AuthModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AuthModal = ({handeClick}) => {
+    return (
+        <section>
+            <div onClick={handeClick}>x</div>
+        </section>
+    );
+};
+
+
+export default AuthModal;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,27 @@
 import { React, useState, useEffect } from 'react';
+import AuthModal from '../components/AuthModal/AuthModal';
 import Navbar from '../components/Navbar/Navbar';
 
 const Home = () => {
 
+    const [showModal, setShowModal] = useState(false);
     const [authToken, isAuthToken] = useState(true);
 
     const handleClick = () => {
         console.log('Clicked');
+        setShowModal(!true);
     }
 
     useEffect(() => {
-        isAuthToken(true);
+        isAuthToken(false);
     }, []);
 
     return (
         <div className='overlay'>
             <Navbar minimal={true} authToken={authToken} />
+            {showModal && (
+                <AuthModal handleClick={handleClick} />
+            )}
             <div>
                 <h1 className="home">Swipe Right&reg;</h1>
                 <button className="primary-button" onClick={handleClick}>
