a3be5305ff443b3cd9f8a629b45e745d214b35e8
Wesley <wesleyadwebster@gmail.com>
2022-05-23 17:16:23 -0500

    react hook form added

--- src/components/AuthModal/AuthModal.js
+++ src/components/AuthModal/AuthModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SignUpForm from '../../containers/SignUpForm/SignUpForm';
+import { smallLogo } from '../../assets';
 import classes from './AuthModal.module.scss';
 
 const { authModal } = classes;
@@ -8,6 +9,7 @@ const AuthModal = ({ handleClick, isSignUp, setShowModal }) => {
     return (
         <section className={authModal}>
             <div>
+                {smallLogo}
                 {isSignUp ? <h2>Create an account</h2> : <h2>Log in</h2>}
                 <p>By clicking <b>{isSignUp ? 'Create Account' : 'Log In'}</b>, you argree to our terms. Learn how we process your data
                     in our Privacy Policy page.
--- src/containers/SignUpForm/SignUpForm.js
+++ src/containers/SignUpForm/SignUpForm.js
@@ -1,16 +1,38 @@
 import { React } from 'react';
+import { useForm } from "react-hook-form";
+import classes from './SignUpForm.module.scss';
+
+const { generalInfo } = classes;
 
 const SignUpForm = ({ handleClick, setShowModal }) => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Submitted!"')
-        setShowModal(false);
-    }
-    
+    console.log(watch("example")); // watch input value by passing the name of it
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     console.log('Submitted!"')
+    //     setShowModal(false);
+    // }
+
     return (
-        <form  onSubmit={handleSubmit}>
-            <button type="submit">Submit</button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={generalInfo}>
+                {/* register your input into the hook by invoking the "register" function */}
+                <label>Name</label>
+                <input defaultValue="test" {...register("example")} />
+                {/* include validation with required or other standard HTML validation rules */}
+                <input {...register("exampleRequired", { required: true })} />
+                {/* errors will return when field validation fails  */}
+                {errors.exampleRequired && <span>This field is required</span>}
+                <label>Gender</label>
+                <select {...register("gender")}>
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                    <option value="other">other</option>
+                </select>
+            </div>
+            <input type="submit" />
             <button onClick={handleClick}>Close</button>
         </form>
     );
