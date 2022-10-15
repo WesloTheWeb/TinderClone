ac4d09bfd97e80876087fdb728a2a91d1b502ea4
Wesley <wesleyadwebster@gmail.com>
2022-10-14 21:31:16 -0500

    onboarding date picker work

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,11 +1,25 @@
 import { React, useState } from 'react';
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+import ReactDatePicker from "react-datepicker";
 import Navbar from '../components/Navbar/Navbar';
+
+
+const defaultValues = {
+    Native: "",
+    TextField: "",
+    Select: "",
+    ReactSelect: { value: "vanilla", label: "Vanilla" },
+    Checkbox: false,
+    switch: false,
+    RadioGroup: "",
+    numberFormat: 123456789,
+    downShift: "apple"
+};
 
 const Onboarding = (props) => {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-
+    const { register, handleSubmit, watch, control, formState: { errors } } = useForm(defaultValues);
+    // const { handleSubmit, register, reset, control } = useForm({ defaultValues });
 
     return (
         <>
@@ -19,17 +33,38 @@ const Onboarding = (props) => {
                 <form >
                     <section>
                         <label>First Name</label>
-                        <input placeholder="firstName" {...register("firstName", { required: true })} />
+                        <input placeholder="first name" {...register("firstName", { required: true })} />
+                        <label>Last Name</label>
+                        <input placeholder="last Name" {...register("lastName", { required: true })} />
                         <label>Birthday</label>
-                        <input placeholder="MMDDYYYY" {...register("Birthday", { required: true })} />
+                        <div className='field-container'>
+                        <Controller
+                            control={control}
+                            name="ReactDatepicker"
+                            render={({ field }) => (
+                                <ReactDatePicker
+                                  placeholderText='Select date'
+                                  onChange={(date) => field.onChange(date)}
+                                  selected={field.value}
+                                />
+                             )}
+                        />
+                        </div>
+                 
                         <label>Gender</label>
-                        <input placeholder="MMDDYYYY" {...register("Gender", { required: true })} />
+                        <select {...register("gender")}>
+                            <option value="female">female</option>
+                            <option value="male">male</option>
+                            <option value="other">other</option>
+                        </select>
                         <label>Show Gender on my profile</label>
-                        <input placeholder="uwu```" {...register("showGender", { required: true })} />
+                        {/* <input placeholder="uwu```" {...register("showGender", { required: true })} /> */}
+                        <label>Yes</label>
+                        <input type="radio" {...register('showGender', { required: true })} value="yes" />
+                        <label>No</label>
+                        <input type="radio" {...register('showGender', { required: true })} value="no" />
                     </section>
-             
                 </form>
-
             </div>
         </>
     );
